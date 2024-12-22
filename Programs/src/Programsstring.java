e00211b368d48434561038858ef39ad98818020a
rashmijg <rashmiskp222@gmail.com>
2024-12-22 11:05:45 +0530

    second commit

---
+++ Programs/src/Programsstring.java
@@ -0,0 +1,19 @@
+
+public class Programsstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		String s= "malayalam";
+		char Ch[]=s.toCharArray();
+		for (int i=s.length()-1;i>=0;i--)
+		{
+			char str =s.charAt(i);
+			
+			System.out.print(str);
+			}
+		
+		
+	}
+
+}
