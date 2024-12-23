6028b6c69fae9dbb06fe2f2eecea70eaa753fcc1
rashmijg <rashmiskp222@gmail.com>
2024-12-23 19:34:05 +0530

    fourth commit

---
+++ Programs/src/PrimeNumber.java
@@ -0,0 +1,22 @@
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num =11;
+     if(num<0)
+     {
+    	 System.out.println("the number is not prime");
+    	 
+     }
+	if(!((num%2)==0))	
+	{
+   	 System.out.println("the number is  prime");
+    }
+	else
+	{
+	   	 System.out.println("the numbernot  is  prime");
+	 }	
+	}
+
+}
