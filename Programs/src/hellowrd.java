56dfd6a7ddd3fdff7fc9db616c879e3101dae304
rashmijg <rashmiskp222@gmail.com>
2025-06-19 23:22:01 +0530

    fifth commit

--- Programs/src/hellowrd.java
+++ Programs/src/hellowrd.java
@@ -3,7 +3,7 @@ public class hellowrd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello word");
+System.out.println("hey word");
 	}
 
 }
